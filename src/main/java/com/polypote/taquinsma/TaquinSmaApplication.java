01e9616066d64eb231352074dd6ab9f56baede55
Galding <nathan.peyronnet@gmail.com>
2023-06-08 11:46:28 +0200

    initial commit

---
+++ src/main/java/com/polypote/taquinsma/TaquinSmaApplication.java
@@ -0,0 +1,13 @@
+package com.polypote.taquinsma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaquinSmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaquinSmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/polypote/taquinsma/TaquinSmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polypote.taquinsma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaquinSmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
